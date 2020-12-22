0476e825441ee4702c41323fcd9bb2660a184047
Matthew James Harrison <mjsonofharry@gmail.com>
2020-12-22 17:40:07 -0500

    remove scripts

--- scripts/upload-assets.ps1
+++
@@ -1,9 +0,0 @@
-New-Item -ItemType Directory -Force -Path $PSScriptRoot\tmp
-
-Compress-Archive `
-    -Path $PSScriptRoot\..\assets `
-    -DestinationPath $PSScriptRoot\tmp\assets.zip `
-    -CompressionLevel Optimal `
-    -Force
-
-aws s3 cp $PSScriptRoot\tmp\assets.zip s3://harrisonviamonte/assets.zip
